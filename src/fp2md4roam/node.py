5f43b5fc1816564f9bfb5f5ca418619e4c354fc5
Romilly Cocking <romilly.cocking@gmail.com>
2021-12-09 10:10:37 +0000

    Refactoring - about to get rid of Node

--- src/fp2md4roam/node.py
+++ src/fp2md4roam/node.py
@@ -2,13 +2,13 @@ from xml.etree.ElementTree import Element
 
 
 class Node:
-    def __init__(self, map_node: Element, map_directory: str, depth: int = 0):
+    def __init__(self, map_node: Element, map_directory: str=None):
         self.map_node = map_node
-        self.map_directory = map_directory
-        self.depth = depth
+        # self.map_directory = map_directory
 
     def children(self):
-        return [Node(child, self.map_directory, self.depth+1) for child in self.map_node.findall('node')]
+        # return [Node(child, self.map_directory) for child in self.map_node.findall('node')]
+        return [Node(child) for child in self.map_node.findall('node')]
 
     def get(self, name):
         return self.map_node.get(name)
